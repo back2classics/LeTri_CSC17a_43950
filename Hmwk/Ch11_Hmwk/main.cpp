a0228dbd069d9c088878cd362fff59cea76b2c06
Tri Le <tri032le@gmail.com>
2015-04-02 13:18:03 -0700

    Hmwk 3 - Ch 11 Number 9

--- Hmwk/Ch11_Hmwk/main.cpp
+++ Hmwk/Ch11_Hmwk/main.cpp
@@ -10,99 +10,126 @@
 using namespace std;
 
 struct Bureau{
-    char name[20];
-    char tele[13];
-    char topic[50];
+    string name;
+    int tele;
+    string topic;
     float fee;
 };
 
+Bureau* fill(Bureau*, int);
+void output(Bureau*, int);
+void ch11_9();
+
 int main(int argc, char** argv) {
 
-    const int PEOPLE = 2; //number of speakers
-    Bureau speaker[PEOPLE];
+    ch11_9();
+    
+return 0;
+}
+
+Bureau* fill(Bureau *s, int SIZE){
     //Getting speaker's data
-    cout << "Enter the data of the " << PEOPLE << " speakers." << endl;
-    for(int i = 0; i < PEOPLE; i++){
+    Bureau *ptr = new Bureau[SIZE];
+    cout << "Enter the data of the " << SIZE << " speakers." << endl;
+    for(int i = 0; i < SIZE; i++){
         cout << "Name of speaker " << (i + 1) << ": ";
-        cin.getline(speaker[i].name, 20);
-        cout << "Telephone number of speaker " << (i + 1) << " (Be sure to"
-                " include the '-'s in the phone number): ";
-        cin.getline(speaker[i].tele, 13);
+        cin >> ptr[i].name;
+        cout << "Telephone number of speaker " << (i + 1) << ": ";
+        cin >> ptr[i].tele;
         cout << "Topic chosen by speaker " << (i + 1) << ": ";
-        cin.getline(speaker[i].topic, 50);
+        cin >> ptr[i].topic;
         cout << "Fee required for speaker " << (i + 1) << ": $";
         do{
-        cin >> speaker[i].fee;
-        cin.ignore();
-        if(speaker[i].fee < 0){
+        cin >> ptr[i].fee;
+        if(ptr[i].fee < 0){
             cout << "Only positive values are valid, please input again: $";
         }
         }
-        while(speaker[i].fee < 0);
+        while(ptr[i].fee < 0);
         cout << endl;
     }
-    cout << "Is the information you provided correct?" << endl;
-    cout << "Enter [Y] or [N]: ";
-    char correct;
-    int num = 0; //speaker's number if incorrect information was provided
-    char isIt;
-    cin >> correct;
-    cin.ignore();
-    if(tolower(correct) == 'y'){
-    //Display the speaker's data
-    for(int i = 0; i < PEOPLE; i++){
+    return ptr;
+}
+void output(Bureau *s, int SIZE){
+    for(int i = 0; i < SIZE; i++){
         cout << "Speaker " << (i + 1) << ": " << endl;
-        cout << "Name: " << speaker[i].name << endl;
-        cout << "Phone Number: " << speaker[i].tele << endl;
-        cout << "Topic: " << speaker[i].topic << endl;
-        cout << "Fee Required: $" << speaker[i].fee << endl;
+        cout << "Name: " << s[i].name << endl;
+        cout << "Phone Number: " << s[i].tele << endl;
+        cout << "Topic: " << s[i].topic << endl;
+        cout << "Fee Required: $" << s[i].fee << endl;
         cout << "_____________________________________________" << endl;
     }
-    }
-    else{
+}
+void ch11_9(){
+    const int PEOPLE = 3; //number of speakers
+    Bureau *speaker = new Bureau[PEOPLE];
+    int num;
+    char isIt;
+    int input;
+    char yes;
+    speaker = fill(speaker, PEOPLE);
+    do{
+    do{
+        cout << "Enter [1] to make changes to speaker's information: " << endl;
+        cout << "Enter [2] to display all of the speaker's information: " << endl;
+        cout << "Enter [3] to exit the program: " << endl;
+        cin >> input;
+        cin.clear();
+        cin.ignore();
         do{
+        if(input != 1 && input != 2 && input != 3)
+        {
+            cout << "Invalid input, please enter again: ";
+            cin >> input;
+            cin.clear();
+            cin.ignore();
+        }
+        }while(input != 1 && input != 2 && input != 3);
+        cout << endl;
+    }while(input != 1 && input != 2 && input != 3);
+    if(input == 1){
         cout << "Enter the speaker's number that is to be corrected: ";
         do{
         cin >> num;
-        cin.ignore();
         if(num > PEOPLE)
         {
             cout << "Invalid speaker number, please enter again: ";
         }
-        }while(num < PEOPLE);
+        }while(num > PEOPLE);
+        do{
         cout << "Name of speaker " << num << ": ";
-        cin.getline(speaker[num].name, 20);
-        cout << "Telephone number of speaker " << num << " (Be sure to"
-                " include the '-'s in the phone number): ";
-        cin.getline(speaker[num].tele, 13);
+        cin >> speaker[num - 1].name;
+        cout << "Telephone number of speaker " << num << ": ";
+        cin >> speaker[num - 1].tele;
         cout << "Topic chosen by speaker " << num << ": ";
-        cin.getline(speaker[num].topic, 50);
+        cin >> speaker[num - 1].topic;
         cout << "Fee required for speaker " << num << ": $";
         do{
-        cin >> speaker[num].fee;
-        cin.ignore();
-        if(speaker[num].fee < 0)
+        cin >> speaker[num - 1].fee;
+        if(speaker[num - 1].fee < 0)
         {
             cout << "Only positive values are valid, please input again: $";
         }
         }
-        while(speaker[num].fee < 0);
+        while(speaker[num - 1].fee < 0);
         cout << endl;
         cout << "Is this information correct?" << endl;
         cout << "Enter [Y] or [N]: ";
         cin >> isIt;
+        cout << endl;
         }while(tolower(isIt) == 'n');
-        }
-    if(tolower(isIt) == 'y'){
-        //Display the speaker's corrected data
-        cout << "Speaker " << num << ": " << endl;
-        cout << "Name: " << speaker[num].name << endl;
-        cout << "Phone Number: " << speaker[num].tele << endl;
-        cout << "Topic: " << speaker[num].topic << endl;
-        cout << "Fee Required: $" << speaker[num].fee << endl;
-        cout << "_____________________________________________" << endl;
     }
+    else if(input == 2){
+        output(speaker, PEOPLE);
+    }
+    else{
+        cout << "Are you sure you are done?" << endl;
+    }
+    cout << "Would you like to go back to the main menu?" << endl;
+    cout << "Enter [Y] or [N]: ";
+    cin >> yes;
+    }while(tolower(yes) == 'y');
     
-    return 0;
+    delete [] speaker;
 }
 
