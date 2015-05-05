3bf9f75d266388cbb83f7bf64f404a7a65f37e80
Tri Le <tri032le@gmail.com>
2015-05-04 21:00:52 -0700

    Project 1 COMPLETED

--- commit.bat
+++ commit.bat
@@ -1,5 +1,5 @@
 git add --all
 git config --global user.email "tri032le@gmail.com"
 git config --global user.name "Tri Le"
-git commit -m "Project 1"
+git commit -m "Project 1 COMPLETED"
 git push origin master
