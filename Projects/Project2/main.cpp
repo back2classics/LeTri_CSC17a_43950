b37fbbf34d87aa4eab31187e285e86296310f86b
back2classics <Tri@Tris-iMac.local>
2015-05-29 21:10:53 -0700

    Project 2 - Player Overlapping Fixed
    Player’s ships cannot be overlapped. Coordinates to be entered again if
    overlapping is detected.

--- Projects/Project2/main.cpp
+++ Projects/Project2/main.cpp
@@ -48,127 +48,124 @@ int main(int argc, char** argv) {
 //    }while(tolower(valid[0] == 'n'));
 //    cout << player.getName() << ", give your crew orders!" << endl;
 //    cout << player.getName() << "'s turn will now begin." << endl;
-//    cout << "This is what your board looks like! Plan accordingly!" << endl;
-//    player.output(); //Board preview before ship placement
+    cout << "This is what your board looks like! Plan accordingly!" << endl;
+    player.output(); //Board preview before ship placement
     //Player's ship placement begins here
-//    for(int i=0;i<5;i++){
-//        if(i == 0){
-//            temp = player.getShip1();
-//        }
-//        else if(i == 1){
-//            temp = player.getShip2();
-//        }
-//        else if(i == 2){
-//            temp = player.getShip3();
-//        }
-//        else if(i == 3){
-//            temp = player.getShip4();
-//        }
-//        else{
-//            temp = player.getShip5();
-//        }
-//        cout << "The ship that is going to be placed is of size " << temp << "." << endl;
-//        player.setYaxis();
-//        player.setXaxis();
-//        player.setPos();
-//        player.checkPos(temp);
-//        player.setShip(temp);
-//        cout << endl;
-//    }
-    //cout << player.getName() << "'s battle preparations have been complete!" << endl;
-//    cout << "Your updated board looks like this!" << endl;
-//    player.output();
+    for(int i=0;i<5;i++){
+        if(i == 0){
+            temp = player.getShip1();
+        }
+        else if(i == 1){
+            temp = player.getShip2();
+        }
+        else if(i == 2){
+            temp = player.getShip3();
+        }
+        else if(i == 3){
+            temp = player.getShip4();
+        }
+        else{
+            temp = player.getShip5();
+        }
+        cout << endl;
+        cout << "The ship that is going to be placed is of size " << temp << "." << endl;
+        player.setYaxis();
+        player.setXaxis();
+        player.setPos();
+        player.checkPos(temp);
+        player.isOverlap(temp);
+        if(player.getOverlap() == true){
+            do{
+                cout << endl;
+                cout << "The coordinates chosen will make the ship overlap with"
+                        " another ship." << endl;
+                player.setYaxis();
+                player.setXaxis();
+                player.setPos();
+                player.checkPos(temp);
+                player.isOverlap(temp);
+            }while(player.getOverlap() == true);
+        }
+        player.setShip(temp);
+        cout << endl;
+        cout << "The ship has been placed! Your updated board looks like this!" << endl;
+        player.output();
+    }
+    cout << player.getName() << "'s battle preparations have been complete!" << endl;
+    cout << "Here is your board after all the ships have been placed!" << endl;
+    player.output();
 //    cout << player.getName() << "'s crew is ready and prepared for battle!" << endl;
 //    cout << player.getName() << "'s turn has ended." << endl;
 //    //Enemy's ship placement starts here!
 //    cout << "Scallywag " << enemy.getName() << " has begun giving his/her orders!" << endl;
 //    cout << enemy.getName() << "'s turn will now begin." << endl;
-    for(int i=0; i<5; i++){
-        cout << "Generating enemy battlefield..." << endl;
-        if(i == 0){
-            temp = enemy.getShip1();
-        }
-        else if(i == 1){
-            temp = enemy.getShip2();
-        }
-        else if(i == 2){
-            temp = enemy.getShip3();
-        }
-        else if(i == 3){
-            temp = enemy.getShip4();
-        }
-        else{
-            temp = enemy.getShip5();
-        }
-        enemy.setYaxis(rand() % 10); //Stores yTemp in enemy private member
-        enemy.setXaxis(rand() % 10); //Stores xTemp in enemy private member
-        tempPos = rand() % 2;
-        if(tempPos == 0)
-        {
-            posTemp = 'h';
-            enemy.setPos(posTemp);
-        }
-        else
-        {
-            posTemp = 'v';
-            enemy.setPos(posTemp);
-        }
-        enemy.checkE(temp); //If rand() fails then check reassigns private members
-        enemy.isOverlap(temp); //Should always be passed on first loop
-        if(enemy.getOverlap() == true){
-            do{
-        enemy.setYaxis(rand() % 10); //Stores yTemp in enemy private member
-        enemy.setXaxis(rand() % 10); //Stores xTemp in enemy private member
-        tempPos = rand() % 2;
-        if(tempPos == 0)
-        {
-            posTemp = 'h';
-            enemy.setPos(posTemp);
-        }
-        else
-        {
-            posTemp = 'v';
-            enemy.setPos(posTemp);
-        }
-        enemy.checkE(temp); //If rand() fails then check reassigns private members
-        enemy.isOverlap(temp); //Should always be passed on first loop
-            }while(enemy.getOverlap() == true);
-//            if(enemy.getPos() == 'h'){
-//                if(enemy.getYaxis() <= 8){
-//                    yTemp = enemy.getYaxis() + 1;
+//    for(int i=0; i<5; i++){
+//        cout << "Generating enemy battlefield..." << endl;
+//        if(i == 0){
+//            temp = enemy.getShip1();
+//        }
+//        else if(i == 1){
+//            temp = enemy.getShip2();
+//        }
+//        else if(i == 2){
+//            temp = enemy.getShip3();
+//        }
+//        else if(i == 3){
+//            temp = enemy.getShip4();
+//        }
+//        else{
+//            temp = enemy.getShip5();
+//        }
+//        enemy.setYaxis(rand() % 10); //Stores yTemp in enemy private member
+//        enemy.setXaxis(rand() % 10); //Stores xTemp in enemy private member
+//        tempPos = rand() % 2;
+//        if(tempPos == 0)
+//        {
+//            posTemp = 'h';
+//            enemy.setPos(posTemp);
+//        }
+//        else
+//        {
+//            posTemp = 'v';
+//            enemy.setPos(posTemp);
+//        }
+//        enemy.checkE(temp); //If rand() fails then check reassigns private members
+//        enemy.isOverlap(temp); //Should always be passed on first loop
+//        //if block runs if a ship is overlapping with another ship and randomizes again
+//        if(enemy.getOverlap() == true){
+//            do{
+//                enemy.setYaxis(rand() % 10); 
+//                enemy.setXaxis(rand() % 10); 
+//                tempPos = rand() % 2;
+//                if(tempPos == 0)
+//                {
+//                    posTemp = 'h';
+//                    enemy.setPos(posTemp);
 //                }
-//                else{
-//                    yTemp = enemy.getYaxis() - 1;
+//                else
+//                {
+//                    posTemp = 'v';
+//                    enemy.setPos(posTemp);
 //                }
-//                enemy.setYaxis(yTemp);
-//            }
-//            else{
-//                if(enemy.getXaxis() <= 8){
-//                    xTemp = enemy.getXaxis() + 1;
-//                }
-//                else{
-//                    xTemp = enemy.getXaxis() - 1;
-//                }
-//                enemy.setXaxis(xTemp);
-//            }
-//            enemy.spawnE(temp);
-        }
-        enemy.checkE(temp);
-        enemy.spawnE(temp);
-    }
-    cout << enemy.getName() << "'s crew is ready and prepared for battle!" << endl;
-    cout << "For testing purposes!" << endl;
-    enemy.output();
-    //player.setWinLose();
-    enemy.setWinLose();
-    //cout << player.getName() << "'s win/lose condition is if " << player.getWinLose() << " ships sink!" << endl;
-    cout << enemy.getName() << "'s win/lose condition is if " << enemy.getWinLose() << " ships sink!" << endl;
+//                enemy.checkE(temp); 
+//                enemy.isOverlap(temp); 
+//            }while(enemy.getOverlap() == true);
+//        }
+//        enemy.checkE(temp);
+//        enemy.spawnE(temp);
+//    }
+    //cout << enemy.getName() << "'s crew is ready and prepared for battle!" << endl;
+    //cout << "For testing purposes!" << endl;
+    //enemy.output();
+    player.setWinLose();
+    //enemy.setWinLose();
+    cout << player.getName() << "'s win/lose condition is if " << player.getWinLose() << " ships sink!" << endl;
+    //cout << enemy.getName() << "'s win/lose condition is if " << enemy.getWinLose() << " ships sink!" << endl;
     cout << endl;
     //Player's turn to fire at enemy begins here!
-    cout << player.getName() << ", it is your turn!" << endl;
-    cout << player.getName() << ", you can fire at the enemy once!" << endl;
-    cout << "Choose your shot coordinate now!" << endl;
-    
+//    cout << player.getName() << ", it is your turn!" << endl;
+//    cout << player.getName() << ", you can fire at the enemy once!" << endl;
+//    cout << "Choose your shot coordinate now!" << endl;
     
     return 0;
 }
